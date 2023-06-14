9b6579ad8b8d3eb73d270f5026793c8e1770972a
Arthur Brito <aline.nolasco.brito@gmail.com>
2023-06-14 15:50:31 -0300

    comit

---
+++ java.js
@@ -0,0 +1,15 @@
+let Fruta = "maçã";
+
+switch (Fruta) {
+    case "maçã":
+        alert("Não vendemos esta fruta aqui");
+        break;
+    case "kiwi": 
+        alert("Estamos com escassez de kiwis");
+        break;
+    case "melancia": 
+        alert("Aqui está, são 3 reais o quilo");
+        break;
+    default: 
+        alert("erro");
+} 
